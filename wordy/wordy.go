fa1ef3840c1d614e1dc4c13607a3824577b58048
asioson <allan.sioson@gmail.com>
2021-09-25 13:34:47 +0800

    first commit

---
+++ wordy/wordy.go
@@ -0,0 +1,50 @@
+// Package wordy implements a routine to parse and evaluate
+// a math expression in words to an integer value
+package wordy
+
+import (
+    "fmt"
+    "strings"
+)
+
+// Answer takes in a question, parses it, and evaluates the
+// math expression in words to an integer value
+func Answer(question string) (int, bool) {
+    n := len(question)
+    if n < 8 {
+        return 0, false
+    }
+    if question[:8] != "What is " {
+        return 0, false
+    }
+    data := strings.Replace(question[8:n-1],"plus","+", -1)
+    data = strings.Replace(data,"minus","-", -1)
+    data = strings.Replace(data,"multiplied by","*", -1)
+    data = strings.Replace(data,"divided by","/", -1)
+    parsedData := (strings.Split(data," "))
+    m := len(parsedData)
+    if m % 2 == 0 || parsedData[0] == "+" || parsedData[0] == "-" {
+        return 0, false
+    }
+    x := 0
+    _, err := fmt.Sscanf(parsedData[0], "%d", &x)
+    if err == nil && m > 1 {
+        y := 0
+        for i := 1; i < m; i += 2 {
+            op := parsedData[i]
+            _, err = fmt.Sscanf(parsedData[i+1], "%d", &y)
+            if err != nil {
+                return 0, false
+            }
+            switch op {
+            case "+" :  x += y
+            case "-" :  x -= y
+            case "*" :  x *= y
+            case "/" :  x /= y
+            default:
+                return 0, false
+            }
+        }
+    } 
+    return x, true 
+}
