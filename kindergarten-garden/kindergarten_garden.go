e544e4f134f2422969b36d2db54e9a46746f2f39
asioson <allan.sioson@gmail.com>
2021-09-25 11:17:36 +0800

    first commit

--- kindergarten-garden/kindergarten_garden.go
+++ kindergarten-garden/kindergarten_garden.go
@@ -16,53 +16,53 @@ type Garden map[string][]string
 // and returns a new kindergarten garden.
 func NewGarden(diagram string, children []string) (*Garden, error) {
     n := len(children)
-	sortedChildren := make([]string, n)
-	copy(sortedChildren, children)
-	sort.Strings(sortedChildren)
+    sortedChildren := make([]string, n)
+    copy(sortedChildren, children)
+    sort.Strings(sortedChildren)
     for i := 0; i < n - 1; i++ {
-		if sortedChildren[i] == sortedChildren[i+1] {
-			return nil, fmt.Errorf("%s has duplicate", sortedChildren[i])
-		}
-	}
-	if len(diagram) == 0 || diagram[0] != '\n' {
-		return nil, fmt.Errorf("invalid garden format")
-	}
+        if sortedChildren[i] == sortedChildren[i+1] {
+            return nil, fmt.Errorf("%s has duplicate", sortedChildren[i])
+        }
+    }
+    if len(diagram) == 0 || diagram[0] != '\n' {
+        return nil, fmt.Errorf("invalid garden format")
+    }
     row := strings.Split(diagram[1:], "\n") 
     if len(row) != 2 {
-		return nil, fmt.Errorf("invalid garden format")
-	}
+        return nil, fmt.Errorf("invalid garden format")
+    }
     if len(row[0]) != len(row[1]) {
-	    return nil, fmt.Errorf("uneven garden")
+        return nil, fmt.Errorf("uneven garden")
     }
     plantName := map[rune]string{
-	    'C': "clover",
-	    'G': "grass",
-	    'R': "radishes",
-	    'V': "violets",
+        'C': "clover",
+        'G': "grass",
+        'R': "radishes",
+        'V': "violets",
     }
-	garden := Garden{}
+    garden := Garden{}
     for i := 0; i < 2; i++ {
         if 2 * n != len(row[i]) {
-			return nil, fmt.Errorf("each child must have 2 plants per row")
+            return nil, fmt.Errorf("each child must have 2 plants per row")
         }
-		for j, code := range row[i] {
-			name, ok := plantName[code]
-			if !ok {
-				return nil, fmt.Errorf("unknown plant")
-			}
-			child := sortedChildren[j/2]
-			plantList, ok := garden[child]
-			if !ok {
-				plantList = []string{}
-			}
-			garden[child] = append(plantList, name)
-		}
-	}
-	return &garden, nil
+        for j, code := range row[i] {
+            name, ok := plantName[code]
+            if !ok {
+                return nil, fmt.Errorf("unknown plant")
+            }
+            child := sortedChildren[j/2]
+            plantList, ok := garden[child]
+            if !ok {
+                plantList = []string{}
+            }
+            garden[child] = append(plantList, name)
+        }
+    }
+    return &garden, nil
 }
 
 // Plants returns a map where each child maps to a list of plants
 func (g Garden) Plants(child string) ([]string, bool) {
-	plantList, ok := g[child]
-	return plantList, ok 
+    plantList, ok := g[child]
+    return plantList, ok 
 }
